6c525f57500f9cb0fd6432a2791c7caa91d9197e
Biswarup Basak <153805988+biswarup65@users.noreply.github.com>
2026-04-05 23:00:22 +0530

    Delete dockerpush.groovy

--- dockerpush.py
+++
@@ -1,8 +0,0 @@
-def call(String CredentialsID,String Imagename,String Tag){
-  withCredentials([usernamePassword(credentialsId: CredentialsID,usernameVariable: "docker_user",passwordVariable: "docker_pass")]){
-    sh "docker login -u ${env.docker_user} -p ${env.docker_pass}"
-    sh " docker tag two-tier-flask:v3 ${env.docker_user}/${Imagename}:${Tag}"
-    sh " docker push ${env.docker_user}/${Imagename}:${Tag} "
-    echo " Docker Image Pushed Successfully on DockerHub"
-  }
-}
